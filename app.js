f0bfaf2b75319803f5cfa61a2eeeaab639daaf7b
Yongran <maiyongran@gmail.com>
2022-09-28 15:30:37 +0100

    add flags

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const fetchCountries = async () => {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const jsonData = await response.json();
     
-    const countriesNames = jsonData.map( country => country.name).flat();
-    
-    countriesNames.forEach(countryName => {
+    jsonData.forEach(country => {
         const listItem = document.createElement("li");
-        listItem.innerText = countryName.common;
+
+        const flag = country.flag;
+
+        const span1 = document.createElement("span");
+        span1.innerText = flag;
+
+        const name = country.name;
+
+        const span2 = document.createElement("span");
+        span2.innerText = name.common;
+
+        listItem.appendChild(span1);
+        listItem.appendChild(span2);
         ul.appendChild(listItem);
     })
 }
